d8ea1bd1e9a88f6e8476579355eb26f36f008a56
inwoo <inwooha00@naver.com>
2025-09-16 12:51:57 +0900

    project init

---
+++ src/main/java/io/github/herbpot/miyobackend/MiYoBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.herbpot.miyobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiYoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiYoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/herbpot/miyobackend/MiYoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.herbpot.miyobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiYoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
