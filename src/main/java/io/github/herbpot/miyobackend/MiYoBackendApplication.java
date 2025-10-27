79e20650be75cd84697cb56d082e2ea44d533d04
dasom040819 <da1ce10819@gmail.com>
2025-10-28 00:43:27 +0900

    build: 빌드 파일 추가

--- src/main/java/io/github/herbpot/miyobackend/MiYoBackendApplication.java
+++ src/main/java/io/github/herbpot/miyobackend/MiYoBackendApplication.java
@@ -2,8 +2,10 @@ package io.github.herbpot.miyobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MiYoBackendApplication {
 
     public static void main(String[] args) {
