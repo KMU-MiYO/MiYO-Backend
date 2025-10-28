8730b4f8719cac30bc4d4377181e11299782850d
dasom040819 <da1ce10819@gmail.com>
2025-10-29 00:27:37 +0900

    fix: RedisConfig 수정

--- src/main/java/io/github/herbpot/miyobackend/config/RedisConfig.java
+++ src/main/java/io/github/herbpot/miyobackend/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -24,6 +25,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * - 메시지 리스너 컨테이너 구성
  */
 @Configuration
+@EnableRedisRepositories(basePackages = "io.github.herbpot.miyobackend.redis.repository")
 public class RedisConfig {
 
     /**
