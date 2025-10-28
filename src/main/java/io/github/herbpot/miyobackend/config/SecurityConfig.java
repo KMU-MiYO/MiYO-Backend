53079f07eece5fa03e4b5f844d5f334e6ded7de7
inwoo <inwooha00@naver.com>
2025-10-28 12:52:14 +0900

    [FIX]: Allow Option Method for all router / delete allow creditional

--- src/main/java/io/github/herbpot/miyobackend/config/SecurityConfig.java
+++ src/main/java/io/github/herbpot/miyobackend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import io.github.herbpot.miyobackend.global.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -47,6 +48,7 @@ public class SecurityConfig {
                                 "/dev/users/swagger-ui/**",
                                 "/dev/users/v3/api-docs/**"
                         ).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
--- src/main/java/io/github/herbpot/miyobackend/config/WebConfig.java
+++ src/main/java/io/github/herbpot/miyobackend/config/WebConfig.java
@@ -10,7 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowCredentials(true)
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3600);
