d707ef7d7fe3789584c74c0c6e1b1470b68b9b64
dasom040819 <da1ce10819@gmail.com>
2025-10-30 02:56:01 +0900

    fix: key 수정

--- src/main/java/io/github/herbpot/miyobackend/config/SecurityConfig.java
+++ src/main/java/io/github/herbpot/miyobackend/config/SecurityConfig.java
@@ -51,7 +51,6 @@ public class SecurityConfig {
                         .requestMatchers("/", "/actuator/**").permitAll()
 
                         // Swagger UI 및 OpenAPI 문서는 인증 없이 접근 가능
-                        .requestMatchers("/dev/challenge/swagger-ui/**", "/dev/challenge/v3/api-docs/**", "/dev/challenge/swagger-ui.html").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
 
                         // 공모전 목록 조회는 인증 불필요, 상세 조회는 인증 필요
